c7ca2506977e319bb73d2b1ca892d924f86b6c10
Caeldeth <sabrael@hybrasyl.com>
2021-10-24 03:21:23 -0500

    begin work on markdown generation

--- index.js
+++ index.js
@@ -1,119 +1,128 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const generateMD = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        message: "What is the project title?",
-        name: "title",
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('You must enter a project title!');
-                return false;
-            }
-        }
-    },
-    {
-        type: "input",
-        message: "What is the project description?",
-        name: "description",
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('You must enter a project description!');
-                return false;
-            }
-        }
-    },
-    {
-        type: "checkbox",
-        message: "What optional sections would you like to add?",
-        name: "sections",
-        // don't give options for description, table of contents, installation, usage, license, questions
-        choices: ['Contributing', 'Tests']
-    },
-    {
-        type: "input",
-        message: "What are the steps for installation?",
-        name: "installsteps"
-    },
-    {
-        type: "input",
-        message: "What is the usage information?",
-        name: "usage"
-    },
-// Conditional for selecting contribution guidelines based on optional sections checkbox value
-    {
-        type: "list",
-        message: "Would you like to include the Contributor Covenant or custom contribution guidelines?",
-        name: "glconfirm",
-        choices: ['Contributor Covenant', 'Custom'],
-        default: 'Contributor Covenant',
-        when: ({ sectionsCheckbox }) => sectionsCheckbox.includes('Contributor')
-    },
-    {
-        type: "input",
-        message: "What are your custom contribution guidelines?",
-        name: "customguidelines",
-        validate: customguidelines => {
-            if (customguidelines) {
-                return true;
-            } else {
-                console.log('You must enter a custom guidelines!');
-                return false;
-            }
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the project title? (Required)",
+            name: "title",
+            validate: (titleInput) => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log("You must enter a project title!");
+                    return false;
+                }
+            },
         },
-        when: ({ glconfirmList }) => glconfirmList.includes('Custom')
-    },
-// Conditionals for test steps if included
-    {
-        type: "input",
-        message: "What are the test instructions?",
-        name: "teststeps",
-        validate: teststepsInput => {
-            if (teststepsInput) {
-                return true;
-            } else {
-                console.log('You must enter a custom guidelines!');
-                return false;
-            }
+        {
+            type: "input",
+            message: "What is the project description? (Required)",
+            name: "description",
+            validate: (descriptionInput) => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("You must enter a project description!");
+                    return false;
+                }
+            },
         },
-        when: ({ sectionsCheckbox }) => sectionsCheckbox.includes('Contributor')
-    },
-// License selection
-    {
-        type: "list",
-        message: "what license should the project have?",
-        name: "license",
-        choices: [
-            'Mozilla Public License 2.0',
-            'MIT',
-            'Apache 2.0'
-        ]
-    },
-// User info
-    {
-        type: "input",
-        message: "What is your github username?",
-        name: "username"
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "email"
-    },    
-];
+        {
+            type: "checkbox",
+            message: "What optional sections would you like to add?",
+            name: "sections",
+            // don't give options for description, table of contents, installation, usage, license, questions
+            choices: ["Contributing", "Tests"],
+        },
+        {
+            type: "input",
+            message: "What are the steps for installation?",
+            name: "installsteps",
+        },
+        {
+            type: "input",
+            message: "What is the usage information?",
+            name: "usage",
+        },
+        // Conditional for selecting contribution guidelines based on optional sections checkbox value
+        {
+            type: "list",
+            message: "Would you like to include the Contributor Covenant or custom contribution guidelines?",
+            name: "glconfirm",
+            choices: ["Contributor Covenant", "Custom"],
+            default: "Contributor Covenant",
+            when: ({ sectionsCheckbox }) => sectionsCheckbox.includes("Contributor"),
+        },
+        {
+            type: "input",
+            message: "What are your custom contribution guidelines?",
+            name: "customguidelines",
+            validate: (customguidelines) => {
+                if (customguidelines) {
+                    return true;
+                } else {
+                    console.log("You must enter a custom guidelines!");
+                    return false;
+                }
+            },
+            when: ({ glconfirmList }) => glconfirmList.includes("Custom"),
+        },
+        // Conditionals for test steps if included
+        {
+            type: "input",
+            message: "What are the test instructions?",
+            name: "teststeps",
+            validate: (teststepsInput) => {
+                if (teststepsInput) {
+                    return true;
+                } else {
+                    console.log("You must enter a custom guidelines!");
+                    return false;
+                }
+            },
+            when: ({ sectionsCheckbox }) => sectionsCheckbox.includes("Contributor"),
+        },
+        // License selection
+        {
+            type: "list",
+            message: "what license should the project have?",
+            name: "license",
+            choices: ["Mozilla 2.0", "MIT", "Apache 2.0"],
+        },
+        // User info
+        {
+            type: "input",
+            message: "What is your github username?",
+            name: "username",
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+        },
+    ])
+    .then(answers => {
+        return generateMarkdown(answers);
+    })
+    .then(pageMarkdown => {
+        return writeToFile(pageMarkdown);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+}
 
 // Function call to initialize app
-init();
+init() 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,49 @@
+const licenseArr = ["Mozilla 2.0", "MIT", "Apache 2.0"];
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === licenseArr[0]) {
+        return `[![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL%202.0-orange?style=plastic&logo=appveyor](https://www.mozilla.org/en-US/MPL/2.0/)`;
+    } else if (license === licenseArr[1]) {
+        return `[![License: MIT License](https://img.shields.io/badge/License-Apache%202.0-yellow?style=plastic&logo=appveyor](https://opensource.org/licenses/MIT)`;
+    } else if (license === licenseArr[1]) {
+        return `[![License: Apache License 2.0](https://img.shields.io/badge/License-MIT-blue?style=plastic&logo=appveyor](https://www.apache.org/licenses/LICENSE-2.0)`;
+    } else {
+        return "";
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === licenseArr[0]) {
+        return `[${licenseArr[0]}](https://www.mozilla.org/en-US/MPL/2.0/)`;
+    } else if (license === licenseArr[1]) {
+        return `[${licenseArr[1]}](https://opensource.org/licenses/MIT)`;
+    } else if (license === licenseArr[2]) {
+        return `[${licenseArr[2]}](https://www.apache.org/licenses/LICENSE-2.0)`;
+    } else {
+        return "";
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === licenseArr[0]) {
+        return `Read more about ${licenseArr[0]} here:`;
+    } else if (license === licenseArr[1]) {
+        return `Read more about ${licenseArr[1]} here:`;
+    } else if (license === licenseArr[2]) {
+        return `Read more about ${licenseArr[2]} here:`;
+    } else {
+        return "";
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title}
 
 `;
 }

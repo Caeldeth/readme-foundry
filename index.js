0562cec4e7633cf900c525478729ba574c827245
Caeldeth <sabrael@hybrasyl.com>
2021-10-24 02:30:13 -0500

    set up inquirer questions

--- index.js
+++ index.js
@@ -7,19 +7,106 @@ const questions = [
     {
         type: "input",
         message: "What is the project title?",
-        name: "title"
+        name: "title",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You must enter a project title!');
+                return false;
+            }
+        }
     },
     {
         type: "input",
         message: "What is the project description?",
-        name: "description"
+        name: "description",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You must enter a project description!');
+                return false;
+            }
+        }
     },
     {
         type: "checkbox",
-        message: "What are the contents?",
-        name: "table",
-        choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+        message: "What optional sections would you like to add?",
+        name: "sections",
+        // don't give options for description, table of contents, installation, usage, license, questions
+        choices: ['Contributing', 'Tests']
     },
+    {
+        type: "input",
+        message: "What are the steps for installation?",
+        name: "installsteps"
+    },
+    {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage"
+    },
+// Conditional for selecting contribution guidelines based on optional sections checkbox value
+    {
+        type: "list",
+        message: "Would you like to include the Contributor Covenant or custom contribution guidelines?",
+        name: "glconfirm",
+        choices: ['Contributor Covenant', 'Custom'],
+        default: 'Contributor Covenant',
+        when: ({ sectionsCheckbox }) => sectionsCheckbox.includes('Contributor')
+    },
+    {
+        type: "input",
+        message: "What are your custom contribution guidelines?",
+        name: "customguidelines",
+        validate: customguidelines => {
+            if (customguidelines) {
+                return true;
+            } else {
+                console.log('You must enter a custom guidelines!');
+                return false;
+            }
+        },
+        when: ({ glconfirmList }) => glconfirmList.includes('Custom')
+    },
+// Conditionals for test steps if included
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "teststeps",
+        validate: teststepsInput => {
+            if (teststepsInput) {
+                return true;
+            } else {
+                console.log('You must enter a custom guidelines!');
+                return false;
+            }
+        },
+        when: ({ sectionsCheckbox }) => sectionsCheckbox.includes('Contributor')
+    },
+// License selection
+    {
+        type: "list",
+        message: "what license should the project have?",
+        name: "license",
+        choices: [
+            'Mozilla Public License 2.0',
+            'MIT',
+            'Apache 2.0'
+        ]
+    },
+// User info
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },    
 ];
 
 // TODO: Create a function to write README file

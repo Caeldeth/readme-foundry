30febd164ab848a01b95543cb3fa2601eec6e729
Caeldeth <sabrael@hybrasyl.com>
2021-10-24 01:28:56 -0500

    push to main to start dev

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMD = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the project description?",
+        name: "description"
+    },
+    {
+        type: "checkbox",
+        message: "What are the contents?",
+        name: "table",
+        choices: ['Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions']
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
